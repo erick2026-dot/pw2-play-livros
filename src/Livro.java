3b477bf874ec89b14c23a8f37dc96d8ae3c4ebe2
erick2026-ponto <erick.yauli17@gmail.com>
2026-03-16 07:59:25 -0300

    Criação da classe Livro

---
+++ src/Livro.java
@@ -0,0 +1,13 @@
+import java.time.LocalTime;
+
+public class Livro {
+    String titulo;
+    LocalTime duracao;
+    String autor;
+    String resumo;
+    String editora;
+    String narrador;
+    String versao;
+    double preco;
+
+}
